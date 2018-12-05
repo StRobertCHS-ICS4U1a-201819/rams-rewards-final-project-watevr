098580a0b918995f5e8a0457d9b28cf0431cbf9d
sidwich <39706045+sidwich@users.noreply.github.com>
2018-12-05 09:47:12 -0500

    make few changes

---
+++ main/Classroom.py
@@ -0,0 +1,16 @@
+import Students
+import Teachers
+
+
+class Classroom(object):
+	"""docstring for Classroom"""
+	def __init__(self):
+		self.student_list = []
+	
+	def add_student(self, student):
+		self.student_list.append(student)
+
+	def 
+
+		
+		
---
+++ main/Teachers.py
@@ -0,0 +1,7 @@
+class Teacher(object):
+	"""docstring for Teacher"""
+	def __init__(self, id, password):
+		self.teacher_id = id
+		self.teacher_password = password
+	
+		
