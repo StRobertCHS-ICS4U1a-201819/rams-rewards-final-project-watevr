adfa7f875dd536a23514d4b22b0eba139750722d
彦辰 郭 <guo857389149@126.com>
2018-12-05 16:31:48 -0500

    added teacher and students

--- main/Students.py
+++ main/Students.py
@@ -1,9 +1,13 @@
 class Student(object):
 	"""docstring for Student"""
-	def __init__(self, name, id, homeRoom, points = 0):
+	def __init__(self, name, id, homeRoom, points = 0):         #name = str; id = int; homeRoom = str                              
 		self.student_name = name
 		self.student_id = id
 		self.student_homeRoom = homeRoom
 		self.student_points = points
 
-justin = Student("justin", 123, "cs")
+justin = Student("justin", 2, "cs")
+evan = Student("evan", 1, "cs")
+jerry = Student("jerry", 3, "cs")
+warden = Student("jerry", 4, "cs")
+alvin = Student("alvin", 5, "cs")
