78aed6a46ce7f8f5102abc893559686ce315aacb
sidwich <39706045+sidwich@users.noreply.github.com>
2018-12-07 09:45:23 -0500

    add method: get teacher

--- main/Teachers.py
+++ main/Teachers.py
@@ -1,7 +1,9 @@
 class Teacher(object):
 	"""docstring for Teacher"""
-	def __init__(self, id, password):            #id = int; password = str      
-		self.teacher_id = id
+	def __init__(self, first_name, last_name, ID, password):          #id = int; password = str      
+		self.teacher_fistName = first_name
+		self.teacher_lastName = last_name
+		self.teacher_id = ID
 		self.teacher_password = password
 
 MrFabroa = Teacher(1, "admin")
