62d2f4b732fb3b09331e88665915ba5356e38df2
Jerry123455 <JiangShan.Cui18@ycdsbk12.ca>
2018-12-08 12:43:21 -0600

    Commits proper file organization

--- RRTAA/Resources/kivytut2.py
+++ RRTAA/Resources/kivytut2.py
@@ -5,6 +5,9 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+
+
+
 Builder.load_string('''
 <ScreenOne>:
     BoxLayout:
@@ -14,6 +17,40 @@ Builder.load_string('''
                 root.manager.transition.direction = "left"
                 root.manager.transition.duration = 1
                 root.manager.current = "screen_two"
+    
+<CustomPopup>:
+    size_hint: .5, .5
+    auto_dismiss: False
+    title: "Reward Activities"
+    Button:
+        text: "Highest Mark"
+        on_press: root.dismiss()
+    Button:
+        text: "Average over 85"
+        on_press: root.dismiss()
+    Button:
+        text: "Club Meeting"
+        on_press: root.dismiss()
+    Button:
+        text: "Sport Team"
+        on_press: root.dismiss()
+    Button:
+        text: "Activities Leader"
+        on_press: root.dismiss()
+    
+    BoxLayout:
+        orientation: "horizontal"
+        height: 30
+        
+        BoxLayout:
+            orientation: "horizontal"
+            size_hint_x: .25
+
+            Spinner:
+                text: "Reward Activities"
+                values: ["Highest Mark", "Average over 85", "Club Meeting", "Sport Team", "Activities Leader"]
+                id: spinner_id
+                on_text: root.spinner_clicked(spinner_id.text)
 
 <ScreenTwo>:
     BoxLayout:
