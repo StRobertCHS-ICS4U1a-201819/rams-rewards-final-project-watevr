b4a662204824ef3ca0c913ad9cfbe53cbb5aa5b1
Meitnerium109 <andrewlau44@yahoo.ca>
2018-12-12 22:59:17 -0500

    add student info to popup

--- rrsa/rrsa.py
+++ rrsa/rrsa.py
@@ -6,9 +6,12 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 
 class Rrsa(BoxLayout):
+    #testing lists by putting in placeholders
     student_list = ["Bob Marley", "Bob Bob", "Bobby Bob"]
     student_password_list = ["Bob123", "Bob321", "Bob231"]
     student_reward_points = [0, 0, 0]
+    student_homeroom = [None, None, None]
+    student_id = [123, 321, 213]
     full_name_text_input = ObjectProperty()
     password_text_input = ObjectProperty()
 
@@ -17,8 +20,7 @@ class Rrsa(BoxLayout):
         student_password = self.password_text_input.text
         content = Button(text='Click here to escape')
         popup = Popup(title='Login Successful to ' + student_name, title_size=30,
-                      content=content,
-                      size_hint=(.3, .3), size=(400, 400))
+                      content=content, size_hint=(.3, .3), size=(400, 400))
         content.bind(on_press=popup.dismiss)
         for i in range(len(self.student_list)):
             if self.student_list[i] == student_name and self.student_password_list[i] == student_password:
@@ -36,7 +38,19 @@ class Rrsa(BoxLayout):
                 popup.open()
 
     def view_student_info(self):
-        pass
+        student_name = self.full_name_text_input.text
+        student_password = self.password_text_input.text
+
+        for i in range(len(self.student_list)):
+            if self.student_list[i] == student_name and self.student_password_list[i] == student_password:
+                homeroom = self.student_homeroom[i]
+                id = self.student_id[i]
+
+                popup = Popup(title='Student Info', title_size=32,
+                              content=Label(text='Full Name: ' + student_name + '\n\n Homeroom: ' +
+                              str(homeroom) + '\n\n id: ' + str(id), size_hint_y=.9),
+                              size_hint=(.4, .4), size=(400, 400))
+                popup.open()
 
     def student_barcode(self):
         pass
