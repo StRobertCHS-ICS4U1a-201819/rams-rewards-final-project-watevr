720d19229ce8bd5d4876db2dba28117b79c22ad1
Jerry123455 <JiangShan.Cui18@ycdsbk12.ca>
2018-12-14 01:08:40 -0600

    Connect with student.py

--- RRTAA/Resources/TextInput.py
+++ RRTAA/Resources/TextInput.py
@@ -9,15 +9,19 @@ from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.lang import Builder
 
 from RRTAA.package import RewardActivities
+from RRTAA.package import Student
 
 class Test(TabbedPanel):
-    def spinner_clicked(self, value):
+    def spinner_clicked(self, acitivies_name):
+        value = RewardActivities.reward.get_point_value(acitivies_name)
+        Student.point_reward.set_point_reward(value)
+        print(value, Student.point_reward.point_reward)
 
-        print(RewardActivities.reward.get_point_value(value))
-        print("Spinner Value " + value)
+        print("Spinner Value " + acitivies_name)
 
-    def id_inputted(self, value):
-        print("student id" + value)
+    def id_inputted(self, id):
+        Student.student_list.get_student_object(id)
+        print(Student.student1.point)
 
 class TextInputApp(App):
     def build(self):
--- RRTAA/package/Student.py
+++ RRTAA/package/Student.py
@@ -1,26 +1,49 @@
 class Student(object):
-    def __init__(self, fName, lName):
+    def __init__(self, fName, lName, id, point):
         self.fName = fName
         self.lName = lName
-        self.point = 0
+        self.id = id
+        self.point = point
+
+    def get_fullName(self):
+        return self.fName + self.lName
 
     def add_point(self, value):
         self.point += value
         return self.point
 
 class Student_list(object):
-    def __init__(self, student_list, point_list):
+    def __init__(self, student_list):
         self.student_list = student_list
-        self.point_list = point_list
 
-    def get_point_value(self, student_name):
-        for i in range(len(self.student_list)):
-            if student_name == self.student_list[i]:
-                return self.point_list[i]
+    def add_student(self, new_student):
+        self.student_list.append(new_student)
 
-student1 = ("Justin", "Guo", 0)
-student2 = ("Evan", "Bai", 0)
-student3 = ("Jerry", "Cui", 0)
-student4 = ("Waldon", "Zhang", 0)
+
+
+    def get_student_object(self, id):
+        id_list = id.split()
+        for i in range(len(id_list)):
+            print(student_list.student_list[1])
+            id_value = int(id_list[i])
+            student_list.student_list[id_value].add_point(point_reward.point_reward)
+
+
+
+class Point_reward(object):
+    def __init__(self):
+        self.point_reward = 0
+
+    def set_point_reward(self, value):
+        self.point_reward = value
+
+student1 = Student("Justin", "Guo", "00001", 0)
+student2 = Student("Evan", "Bai","00002", 0)
+student3 = Student("Jerry", "Cui", "00003", 0)
+student4 = Student("Waldon", "Zhang", "00004", 0)
+
+student_list = Student_list([student1, student2, student3, student4])
+
+point_reward = Point_reward()
 
 
