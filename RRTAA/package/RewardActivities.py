89a8ebc34391aee5d6f00d3a7930ff737a1c1d09
Jerry123455 <JiangShan.Cui18@ycdsbk12.ca>
2018-12-14 18:55:35 -0600

    Commits completion of viewing student reward activities

--- RRTAA/package/RewardActivities.py
+++ RRTAA/package/RewardActivities.py
@@ -2,11 +2,15 @@ class Reward_list(object):
     def __init__(self, reward_list, point_list):
         self.reward_list = reward_list
         self.point_list = point_list
+        self.date = ""
 
     def get_point_value(self, activities_name):
 
        for i in range(len(self.reward_list)):
             if activities_name == self.reward_list[i]:
                 return self.point_list[i]
+
+    def set_date(self, val_date):
+        self.date = val_date
 
 reward = Reward_list(["Highest Mark", "Average over 85", "Club Meeting", "Sport Team", "Activities Leader"], [8, 4, 6, 7, 5])
--- RRTAA/package/Student.py
+++ RRTAA/package/Student.py
@@ -1,9 +1,10 @@
 class Student(object):
-    def __init__(self, fName, lName, id, point):
+    def __init__(self, fName, lName, id, point, reward_history_list):
         self.fName = fName
         self.lName = lName
         self.id = id
         self.point = point
+        self.reward_history_list = reward_history_list
 
     def get_fullName(self):
         return self.fName + self.lName
@@ -11,6 +12,9 @@ class Student(object):
     def add_point(self, value):
         self.point += value
         return self.point
+
+    def add_reward_history_list(self, activities):
+        self.reward_history_list.append(activities)
 
 class Student_list(object):
     def __init__(self, student_list):
@@ -27,18 +31,22 @@ class Student_list(object):
 
             id_value = int(id_list[i])
             if id_value == 1:
-                student1.add_point(point_reward.point_reward)
+                student1.add_point(reward_info.point_reward)
+                student1.add_reward_history_list(reward_info.activities)
 
             elif id_value == 2:
-                student2.add_point(point_reward.point_reward)
+                student2.add_point(reward_info.point_reward)
+                student2.add_reward_history_list(reward_info.activities)
 
 
             elif id_value == 3:
-                student3.add_point(point_reward.point_reward)
+                student3.add_point(reward_info.point_reward)
+                student3.add_reward_history_list(reward_info.activities)
 
 
             elif id_value == 4:
-                student4.add_point(point_reward.point_reward)
+                student4.add_point(reward_info.point_reward)
+                student4.add_reward_history_list(reward_info.activities)
 
             else:
                 raise ValueError
@@ -46,20 +54,24 @@ class Student_list(object):
 
 
 
-class Point_reward(object):
+class Reward_info(object):
     def __init__(self):
         self.point_reward = 0
+        self.activities = ""
 
     def set_point_reward(self, value):
         self.point_reward = value
 
-student1 = Student("Justin", "Guo", "00001", 0)
-student2 = Student("Evan", "Bai","00002", 0)
-student3 = Student("Jerry", "Cui", "00003", 0)
-student4 = Student("Waldon", "Zhang", "00004", 0)
+    def set_activities(self, activities):
+        self.activities = activities
+
+student1 = Student("Justin", "Guo", "00001", 0, [])
+student2 = Student("Evan", "Bai","00002", 0, [])
+student3 = Student("Jerry", "Cui", "00003", 0, [])
+student4 = Student("Waldon", "Zhang", "00004", 0, [])
 
 student_list = Student_list([student1, student2, student3, student4])
 
-point_reward = Point_reward()
+reward_info = Reward_info()
 
 
