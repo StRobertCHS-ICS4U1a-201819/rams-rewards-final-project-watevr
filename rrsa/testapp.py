aa09a283299d6e91b8dac1466da6ef6294bafd6c
Meitnerium109 <andrewlau44@yahoo.ca>
2019-01-21 21:19:10 -0500

    create history of rewards activity screen and button to view it

--- rrsa/testapp.py
+++ rrsa/testapp.py
@@ -43,6 +43,27 @@ class HomePage(Screen):
     def logout(self):
         self.parent.current = 'main_screen'
 
+    def to_rewards(self):
+        self.parent.current = 'rewards_history'
+
+class RewardsHistory(Screen):
+    container = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(RewardsHistory, self).__init__(**kwargs)
+        Clock.schedule_once(self.setup_scrollview, 1)
+
+    def setup_scrollview(self, dt):
+        self.container.bind(minimum_height=self.container.setter('height'))
+        self.add_text_inputs()
+
+    def add_text_inputs(self):
+        for x in range(20):
+            self.container.add_widget(Label(text="Reward Activity {}".format(x), size_hint_y=None, height=60))
+
+    def to_homepage(self):
+        self.parent.current = 'homepage'
+
 class TestApp(App):
     title = "RRSA"
 
